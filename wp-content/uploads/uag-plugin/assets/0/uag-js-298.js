7882539621014b372acac34975a232ec9638ce23
nityanavelkar <nitya.navelkar@gmail.com>
2024-09-20 17:11:31 +0530

    New edits

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-298.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-41d1251b', {"layout":"bars","heading":"JavaScript","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":85,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-4c65bf33', {"layout":"bars","heading":"PHP","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":95,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-4937ccd4', {"layout":"bars","heading":"Python","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":75,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+window.addEventListener( 'load', function() {
+	UAGBCounter.init( '.uagb-block-edaa0655', {"layout":"bars","heading":"Photoshop","numberPrefix":"","numberSuffix":"%","startNumber":0,"endNumber":80,"totalNumber":100,"decimalPlaces":0,"animationDuration":1500,"thousandSeparator":",","circleSize":230,"circleStokeSize":8,"isFrontend":true} );
+});
+ });
